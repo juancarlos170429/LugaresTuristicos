b9efeee85cdb609b6af023fcca8ec9f0ed18ca56
Melissa Espejo Franco <melissa_10_118@hotmail.com>
2021-01-17 21:07:35 -0500

    LagunaHumantay

--- src/Lugares/Cusco.jsx
+++ src/Lugares/Cusco.jsx
@@ -31,8 +31,8 @@ function Cusco() {
                         <div class="row g-0">
                             <div class="col-md-8">
                                 <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <h5 class="card-title">Laguna Humantay</h5>
+                                    <p class="card-text">Para llegar aquí se camina por paisajes alto andinos, hasta llegar al pie de la montaña, camino al nevado de Salkantay.</p>
                                     <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                                 </div>
                             </div>
@@ -54,7 +54,7 @@ function Cusco() {
                             </div>
                             <div class="col-md-8">
                                 <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
+                                    <h5 class="card-title">Aguas termales de Cocalmayo</h5>
                                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                                     <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                                 </div>
